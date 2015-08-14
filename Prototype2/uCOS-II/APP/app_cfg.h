b32e130d81a7d1665fda556c95f1f9df3638f5c3
lighta <lightaisme@gmail.com>
2015-08-14 19:58:22 -0400

    Moving stuff around for configuration

--- Prototype2/uCOS-II/APP/app_cfg.h
+++ Prototype2/uCOS-II/APP/app_cfg.h
@@ -19,13 +19,34 @@
 **************************************************************************************************************
 */
 
-#define  APP_TASK_START_STK_SIZE          256
+#ifndef APP_CFG
+	#define APP_CFG
+	#define  APP_TASK_START_STK_SIZE          256
 
-/*
-**************************************************************************************************************
-*                                             TASK PRIORITIES
-**************************************************************************************************************
-*/
+	/*
+	**************************************************************************************************************
+	*                                             TASK PRIORITIES
+	**************************************************************************************************************
+	*/
 
-#define  APP_TASK_START_PRIO                1
-#define  OS_TASK_TMR_PRIO                   28
+	#define  APP_TASK_START_PRIO                1
+	#define  OS_TASK_TMR_PRIO                   28
+
+
+	// @TODO move me into APP_config.h
+	#define DATA_COM   1
+	/** Caractere pour debuter le programe */
+	#define CH_START 's'
+	/** Caractere pour arreter le programe */
+	#define CH_STOP 'x'
+	
+	
+	#define INTERUTP_MODE
+	#define DEBUG
+	
+	//extra option for debug mode
+	#ifdef DEBUG
+		#define SEMAPHORE_DEBUG
+		#define DEBUG_COM  0
+	#endif
+#endif
