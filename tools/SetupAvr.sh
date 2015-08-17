3f4413515392891b73092f94a7a299050352b675
lighta <lightaisme@gmail.com>
2015-08-17 18:55:11 -0400

    More eclipse integration work
    Fix small operation permitted typo.
    NB2 : package eclipse-avr seem best integration atm, altough it rely on
    gcc4.9 which broke our current makefile

--- tools/SetupAvr.sh
+++ tools/SetupAvr.sh
@@ -13,17 +13,18 @@ cd /tmp
 curl -o headers.zip -u anonymous@qk3e7ekm.ftpserver1.acanac.com:anon ftp://ftp.qk3e7ekm.ftpserver1.acanac.com/avr/atmel-headers-6.1.3.1475.zip
 unzip headers.zip
 sudo mv atmel-headers-6.1.3.1475/avr32/ $INST_PATH/avr32/include
-echo 'export PATH=$PATH:$INST_PATH/bin' >> ~/.bash_profile
-source ~/.bash_profile
+echo "export PATH=$PATH:$INST_PATH/bin" >> ~/.bashrc
+source ~/.bashrc
 
 #create symlink for eclipse as he's dumb and annoying
 cd $INST_PATH
-ln -s bin/avr32-gcc bin/avr-gcc
-ln -s avr32/include/avr32/ avr32/include/avr
+sudo ln -s avr32-gcc bin/avr-gcc
+sudo ln -s avr32/ avr32/include/avr
 
 sudo yum install dfu-programmer
 
 #now compile something to try it out
+cd /tmp
 mkdir ./test
 cd ./test
 curl -o main.c -u anonymous@qk3e7ekm.ftpserver1.acanac.com:anon ftp://ftp.qk3e7ekm.ftpserver1.acanac.com/avr/main.c
@@ -31,4 +32,5 @@ avr32-gcc -mpart=uc3a0512 -std=gnu99 -O2 -Wall main.c -o main.elf
 avr32-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature main.elf main.hex
 echo 'Testing result'
 avr32-size -B main.hex
+cd ..
 rm -rf ./test
